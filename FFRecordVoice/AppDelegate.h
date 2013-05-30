0560d3f4e025f4de3c13933dbe3beeb44516bfed
Femina Ernest <femina@Feminas-MacBook-Air.local>
2013-05-30 18:31:41 +0530

    Initial Commit

---
+++ FFRecordVoice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FFRecordVoice
+//
+//  Created by Femina Ernest on 5/30/13.
+//  Copyright (c) 2013 Femina Ernest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
